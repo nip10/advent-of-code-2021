1ba41cba1b24516f055c10936f3bd4ae8598de33
Diogo Cardoso <diogocardoso92@outlook.com>
2021-12-06 23:34:15 +0000

    add another solution for day1-part1

--- day1/day1-part1.js
+++ day1/day1-part1.js
@@ -2,10 +2,21 @@ const fs = require("fs");
 // Read input file into array
 const lines = fs.readFileSync("day1-input.txt", "utf8").toString().split("\n");
 
-let counter = 0;
-for (let i = 1; i < lines.length; i++) {
-  if (Number.parseInt(lines[i - 1], 10) < Number.parseInt(lines[i], 10)) {
-    counter++;
-  }
-}
+// Option 1
+// let counter = 0;
+// for (let i = 1; i < lines.length; i++) {
+//   if (Number.parseInt(lines[i - 1], 10) < Number.parseInt(lines[i], 10)) {
+//     counter++;
+//   }
+// }
+
+// Option 2
+const counter = lines.reduce(
+  (acc, curr, i) =>
+    Number.parseInt(lines[i - 1], 10) < Number.parseInt(curr, 10)
+      ? acc + 1
+      : acc,
+  0
+);
+
 console.log(`Counter: ${counter}`);
